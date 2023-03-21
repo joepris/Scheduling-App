f24f1f0be60c0cb9dcff4bdf7f1e65c95b92980b
JoePris <calpito.jp@gmail.com>
2023-03-20 19:53:58 -0600

    Update as of March 20

---
+++ Nursing Staff Planning and Scheduling Excellence/Models/UserWithScheduleVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NursingStaffPlanningandSchedulingExcellence.Models
+{
+    public class UserWithScheduleVM
+    {
+        public UserVM User { get; set; }
+        public ShiftScheduleVM ShiftSchedule { get; set; } 
+    }
+}
