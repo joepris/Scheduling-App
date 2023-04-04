5b731d7531cfc006de378e9a280955bf485253c6
JoePris <calpito.jp@gmail.com>
2023-04-03 21:34:33 -0600

    changes as of April 3

--- Nursing Staff Planning and Scheduling Excellence/Controllers/HomeController.cs
+++ Nursing Staff Planning and Scheduling Excellence/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ using System.Web.Mvc;
 
 namespace NursingStaffPlanningandSchedulingExcellence.Controllers
 {
+    
     public class HomeController : Controller
     {
         NursingStaffEntities db = new NursingStaffEntities();
